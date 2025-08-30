46907db1cad1120f07cb7c57e2336d98e864ad7c
yonas790 <yoni2752@gmail.com>
2025-08-30 21:29:47 +0300

    Low-Level Caching for Property Queryset

--- properties/urls.py
+++ properties/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
 from . import views
+from django.core.cache import cache
+from .models import Property
 
 urlpatterns = [
     path("properties/", views.property_list, name="property_list"),
 ]
+
+def get_all_properties():
+    # Try to get from Redis cache
+    properties = cache.get("all_properties")
+    if not properties:
+        # If not in cache, fetch from database
+        properties = list(Property.objects.all().values(
+            "id", "title", "description", "price", "location", "created_at"
+        ))
+        # Store in Redis for 1 hour (3600 seconds)
+        cache.set("all_properties", properties, 3600)
+    return properties
